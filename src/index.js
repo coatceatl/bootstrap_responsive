9d5c3f08eb9d5075391290b4d0873e576ec46b42
Oksana Tkachenko <oksana@crystax.net>
2018-07-08 22:08:07 +0300

    added carousel, steel need to change height

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import $ from 'jquery';
-
+import 'bootstrap';
 //import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(document).ready(function(){
+  $('.carousel').carousel({
+    interval: 2000
+  });
+});
