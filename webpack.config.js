2985fb60cd49419fda0c34aa2228aa176810c459
Oksana Tkachenko <oksana@crystax.net>
2018-07-09 03:53:25 +0300

    added background no navbar

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,13 @@ module.exports = {
           'sass-loader'
         ]
       },
+      {
+        test: /\.(png)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+
     ],
   },
   plugins: [
